d47676db6715f43bb684b36b38aec03c73353aae
Steven Than <steventhan11@gmail.com>
2020-02-21 23:06:28 -0500

    updated canonicalize dependencies

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="",
     version="0.0.1",
+    install_requires=[
+        "url-normalize==1.4.1",
+    ],
     author="Steven Than",
     description="Utils for ir homework",
     python_requires='>=3.6'
